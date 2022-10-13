be5c15635df431b83fb8ef91f2d9403964d5daf6
Léo Duggan <leo.duggan@alumni.epfl.ch>
2022-10-13 14:35:29 +0200

    Fixed lying in repo URL, fetching commit author instead

--- .github/repo_regex.sh
+++ .github/repo_regex.sh
@@ -1,12 +1,12 @@
 body_text=$1
 echo $body_text
-regex='github.com/([[[:alnum:]._-]+/[[:alnum:]._-]+)'
+regex='github.com/[[[:alnum:]._-]+/([[:alnum:]._-]+)'
 if [[ $body_text =~ $regex ]]
 then
-  user_repo="${BASH_REMATCH[1]}"
+  user_repo="$2/${BASH_REMATCH[1]}"
   echo "$user_repo"
 else
+  user_repo="REGEX failed"
   exit 1
-  user_repo=""
 fi
 echo "USER_REPO=$user_repo" >> $GITHUB_ENV
